583b682c703346dc41830ae8e907d93fa65d457d
Simonaytes <yandexman228@gmail.com>
2024-02-21 02:19:33 +0300

    Try to update array

--- Src/Arrays.hpp
+++ Src/Arrays.hpp
@@ -2,27 +2,21 @@
 
 #include <stdio.h>
 
-const int kArraySize = 4;
-
+template<int length>
 struct Array
 {
-    int value[kArraySize];
+    int value[length];
+
+    template<int id>
+    void sum_array(const Array<length>& b, Array<length>& c) const
+    {
+        sum_array<id+1>(b, c);
+        c.value[id] = value[id] + b.value[id];
+    };
+
+
+    template<>
+    void sum_array<length>(const Array<length>& b, Array<length>& c) const
+    {
+    }
 };
-
-template <int id>
-void sum_array(const Array& a, const Array& b, Array& c);
-
-template <>
-void sum_array<kArraySize>(const Array& a, const Array& b, Array& c);
-
-template <int id>
-void sum_array(const Array& a, const Array& b, Array& c)
-{
-    sum_array<id+1>(a, b, c);
-    c.value[id] = a.value[id] + b.value[id];
-}
-
-template <>
-void sum_array<kArraySize>(const Array& a, const Array& b, Array& c)
-{
-}
--- Src/main.cpp
+++ Src/main.cpp
@@ -2,7 +2,9 @@
 
 #include "Arrays.hpp"
 
-#include "Matrix.hpp"
+// #include "Matrix.hpp"
+
+const int ArraySize = 4;
 
 void TestInt()
 {
@@ -14,11 +16,11 @@ void TestInt()
 
 void TestArray()
 {
-    Array res_array;
-    const Array a = {0, 1, 2, 3};
-    const Array b = {1, 2, 3, 4};
+    Array<ArraySize> res_array;
+    const Array<ArraySize> a = {0, 1, 2, 3};
+    const Array<ArraySize> b = {1, 2, 3, 4};
     
-    sum_array<0>(a, b, res_array);
+    a.sum_array<0>(b, res_array);
 
     printf("ARRAY:\n");
     for (int i = 0; i < kArraySize; i++)
@@ -26,33 +28,31 @@ void TestArray()
     printf("\n");
 }
 
-void TestMatrix()
-{
+// void TestMatrix()
+// {
+//     Matrix res_matrix;
+//     const Matrix a = {Array{0,   1,  2,  3},
+//                       Array{4,   5,  6,  7},
+//                       Array{8,   9, 10, 11},
+//                       Array{12, 13, 14, 15}};
+//     const Matrix b = {Array{2,   3,  4,  5},
+//                       Array{6,   7,  8,  9},
+//                       Array{10, 11, 12, 13},
+//                       Array{14, 15, 16, 17}};
+//     sum_matrix<0>(a, b, res_matrix);
 
-    Matrix res_matrix;
-    const Matrix a = {Array{0,   1,  2,  3},
-                      Array{4,   5,  6,  7},
-                      Array{8,   9, 10, 11},
-                      Array{12, 13, 14, 15}};
-    const Matrix b = {Array{2,   3,  4,  5},
-                      Array{6,   7,  8,  9},
-                      Array{10, 11, 12, 13},
-                      Array{14, 15, 16, 17}};
-    sum_matrix<0>(a, b, res_matrix);
-
-
-    printf("MATRIX:\n");
-    for (int i = 0; i < RowNumber; i++)
-    {
-        for (int j = 0; j < kArraySize; j++)
-            printf("%d\t", res_matrix.value[i].value[j]);
-        printf("\n");
-    }
-}
+//     printf("MATRIX:\n");
+//     for (int i = 0; i < RowNumber; i++)
+//     {
+//         for (int j = 0; j < kArraySize; j++)
+//             printf("%d\t", res_matrix.value[i].value[j]);
+//         printf("\n");
+//     }
+// }
 
 int main()
 {
     TestInt();
     TestArray();
-    TestMatrix();
+    // TestMatrix();
 }
