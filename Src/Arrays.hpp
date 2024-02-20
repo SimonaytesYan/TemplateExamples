d94ae30ba6270cc13a1aeb4fc5992662292d3c96
Simonaytes <yandexman228@gmail.com>
2024-02-21 01:27:27 +0300

    Array sum trought templates

---
+++ Src/Arrays.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdio.h>
+
+const int kArraySize = 5;
+
+struct Array
+{
+    int value[kArraySize];
+};
+
+template <int id>
+void sum_array(const Array& a, const Array& b, Array& c);
+
+template <>
+void sum_array<kArraySize>(const Array& a, const Array& b, Array& c);
+
+template <int id>
+void sum_array(const Array& a, const Array& b, Array& c)
+{
+    sum_array<id+1>(a, b, c);
+    c.value[id] = a.value[id] + b.value[id];
+}
+
+template <>
+void sum_array<kArraySize>(const Array& a, const Array& b, Array& c)
+{
+}
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,6 +1,37 @@
 #include "Templates.hpp"
 
+#include "Arrays.hpp"
+
+#include "Matrix.hpp"
+
+void TestInt()
+{
+    int res_int = my_sum<int, int>(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    printf("%d\n", res_int);
+}
+
+void TestArray()
+{
+    Array res_array;
+    const Array a = {0, 1, 2, 3, 4};
+    const Array b = {1, 2, 3, 4, 5};
+    
+    sum_array<0>(a, b, res_array);
+
+    for (int i = 0; i < kArraySize; i++)
+        printf("%d ", res_array.value[i]);
+    printf("\n");
+}
+
+// void TestMatrix()
+// {
+//     Matrix res_matrix = {};
+
+// }
+
 int main()
 {
-    volatile int res = my_sum<int, int>(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    TestInt();
+    TestArray();
+    // TestMatrix();
 }
